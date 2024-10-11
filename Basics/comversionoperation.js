5e57b5aa6d2fc5c91bfc298152113ecb0dd6bef5
Atif Afridi <atifafridi378@gmail.com>
2024-10-12 01:21:14 +0500

    init

---
+++ Basics/comversionoperation.js
@@ -0,0 +1,13 @@
+
+let num = 123  
+
+console.log(typeof num , num)
+
+let b = String(num)
+console.log(typeof b , b);
+
+let unknown  = undefined
+console.log(typeof unknown , unknown);
+
+let x = Boolean(unknown)
+console.log(typeof x , x);
--- Basics/varaiables.js
+++ Basics/varaiables.js
@@ -1,10 +1,12 @@
 
 /// In this file we will know how to declare variables and print them 
 
+const age = 21  // once const value is initialized we can not change it later otherwise it give us error at the run time 
 let firstName = "Atif" 
 var lastName = "Afridi"
-
-console.log(firstName, lastName) // Atif Afridi
+ /// midlleName = 'something'  =>   we can also declare variable like that but its not preffer to use
+ 
+console.log(firstName, lastName, age) // Atif Afridi 21
 
 firstName = 'Jack'
 console.log(firstName) // Jack 
@@ -12,9 +14,12 @@ console.log(firstName) // Jack
 lastName = 'willow'
 console.log(lastName) // willow
 
+console.log(age); //21
+
+
 
 /// This will print all the varaibles in the form of table 
 
 console.table([
-    firstName , lastName , firstName , lastName 
+    firstName , lastName , firstName , lastName , age
 ])
