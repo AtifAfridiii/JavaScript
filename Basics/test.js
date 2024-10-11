2e61779ce95dff959d188311cc63abd7b5bdec13
Atif Afridi <atifafridi378@gmail.com>
2024-10-11 23:02:49 +0500

    comment added

--- Basics/test.js
+++ Basics/test.js
@@ -1,4 +1,7 @@
 
+///******************  Note ******************* /
+/// i will do javascript in this repo from very basics
+
 console.log('Atif Afridi'+' '+'Afridi');
 console.log('hello');
 console.log('hello');
