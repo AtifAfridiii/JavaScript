92669ab5ec4ee1f1885b4512818acdf28a606f82
Atif Afridi <atifafridi378@gmail.com>
2024-10-13 00:46:59 +0500

    init

--- Basics/comversionoperation.js
+++ Basics/comversionoperation.js
@@ -1,17 +1,35 @@
 
 let num = 123  
 
-console.log(typeof num , num)
+// console.log(typeof num , num)
 
 let b = String(num)
-console.log(typeof b , b);
+ // console.log(typeof b , b);
 
 let unknown  = undefined
-console.log(typeof unknown , unknown);
+ /// console.log(typeof unknown , unknown);
 
 let val = 'sss'
 
 let x = Boolean(val)
-console.log(typeof x , x);
+// console.log(typeof x , x);
+
+
+/// ******************************** Operations ****************************
+
+let value = 3 
+// console.log(typeof value , -value);
+
+let name1 = ' hsh ' , lastName = ' afridi ' 
+///////// console.log( 123 + 23 +  name1+ 1 + 3 + lastName );
+
+// console.log(2+2)
+// console.log(2+2.5)
+// console.log(3*3)
+// console.log(3**3)
+// console.log(6/3)
+// console.log(9%2);
+
+//console.log((2+3-(-1))-(2*3));
 
 
