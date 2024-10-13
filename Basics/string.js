3fdb8f108dd866879ac6c54e4f04b8ddc635594b
Atif Afridi <atifafridi378@gmail.com>
2024-10-14 00:23:32 +0500

    init

---
+++ Basics/string.js
@@ -0,0 +1,14 @@
+
+const val = new String('hello') 
+const another = ' hello' // => declaring like that or declaring like the above its same doesnt matter 
+
+//console.log(`${val + another}`)  // interpolation
+// console.log(another.length); 
+// console.log(val.repeat(5));
+//console.log(another.replace('hello','hy'))
+// console.log(another.slice(1,4));
+// console.log(another.substring(3))
+// console.log(another.toUpperCase());
+// console.log(another.trim());
+// console.log(another.includes('l'));
+// console.log(another.indexOf('h'));
