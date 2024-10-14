3bc88316d69262bbbd0be8d0136c6830aa51ef1f
Atif Afridi <atifafridi378@gmail.com>
2024-10-14 23:00:45 +0500

    numbers

---
+++ Basics/number_maths.js
@@ -0,0 +1,19 @@
+
+
+// ************************ Numbers ***********************
+
+
+const number = 509 
+// console.log(number);
+ const balance = new Number(33321)
+ // console.log(balance.toString().length);
+ 
+
+ const value = 12.303003
+
+ // console.log(value.toFixed(2));
+ // console.log(value.toLocaleString('en-IN'));
+ // console.log(value.toPrecision(2));
+ 
+ console.log(v);
+ 
