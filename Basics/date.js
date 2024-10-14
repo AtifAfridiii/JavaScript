12f2cf6a8a2dd8c7ae5a137ff27e38d52db6c2b4
Atif Afridi <atifafridi378@gmail.com>
2024-10-15 00:35:31 +0500

    init

---
+++ Basics/date.js
@@ -0,0 +1,19 @@
+
+
+// **************************** Date ************************
+
+let date = new Date()
+ // console.log(date.toDateString());
+// console.log(date.toJSON())
+// console.log(date.toLocaleString())
+
+let newDate = new Date("01 , 7 , 2024")
+// console.log(newDate.toLocaleString());
+
+let anotherDate = new Date(2024 , 0 , 12 ,5,31)
+// console.log(anotherDate.toLocaleString());
+
+let date2 = new Date('01,3,2024')
+
+
+console.log(date2.getMonth()+1);
