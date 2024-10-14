ab1acd0a71767da5215bd32947ded245515eabe3
Atif Afridi <atifafridi378@gmail.com>
2024-10-14 23:21:18 +0500

    math

--- Basics/number_maths.js
+++ Basics/number_maths.js
@@ -14,6 +14,13 @@ const number = 509
  // console.log(value.toFixed(2));
  // console.log(value.toLocaleString('en-IN'));
  // console.log(value.toPrecision(2));
+
  
- console.log(v);
- 
+ // *********************** Math *****************************
+
+ console.log(Math.abs(-3));
+ console.log(Math.round(9.5))
+ console.log(Math.min(2,3,57,8,4,9))
+ console.log(Math.max(2,3,57,8,4,9))
+ console.log(Math.pow(2,2))
+ console.log(Math.random().toPrecision(1))
