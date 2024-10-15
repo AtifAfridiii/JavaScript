1f3eaf973628699fb4c9eb066066b813c8a5ebd4
Atif Afridi <atifafridi378@gmail.com>
2024-10-15 23:53:36 +0500

    init

--- Basics/array.js
+++ Basics/array.js
@@ -4,4 +4,12 @@ let arr = [1,2,3,4,5]
 console.log(arr);
 
 let a = arr 
-console.log(a);
+// console.log(a.length);
+// console.log(a.includes(5));
+// console.log(a.slice(1,3));
+// console.log(a.splice(2,3));
+// console.log(a.push(6));
+// console.log(a.pop());
+// console.log(arr);
+// console.log(a);
+
