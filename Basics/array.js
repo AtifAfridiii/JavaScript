2c620b0c171553c1c791216906973b22442108d2
Atif Afridi <atifafridi378@gmail.com>
2024-10-16 00:39:26 +0500

    init

--- Basics/array.js
+++ Basics/array.js
@@ -2,7 +2,7 @@
 
 let arr = [1,2,3,4,5]
 // let array = [1,2,3,4,5,]
-// let arr2 = [0,10,20,30,40,50,['a','b','c','d','e']]
+ let arr2 = [0,10,20,30,40,50,['a','b','c','d','e']]
 //console.log(arr);
 
 // let a = arr 
@@ -14,11 +14,23 @@ let arr = [1,2,3,4,5]
 // console.log(a.pop());
 // console.log(arr);
 // console.log(a);
-// console.log(array.concat(arr2));
+// let x = array.concat(arr2)
+//  console.log(x);
 // console.log(arr2.reverse());
 // console.log(arr.unshift(7));
 // console.log(arr.shift());
 // console.log(arr);
 // console.log(arr.indexOf(4));
 // console.log(typeof arr , arr.join());
+// console.log(arr2.flat(Infinity));
+
+// console.log(Array.isArray('Atif'));
+// console.log(Array.from('Atif'));
+// console.log();
+
+// let m = 10
+// let n = 20
+// let o = 30
+
+// console.log(Array.of(m,n,o));
 
