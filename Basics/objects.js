bdaf041bb4a99762d264d5f01e754b88d42e045f
Atif Afridi <atifafridi378@gmail.com>
2024-10-18 23:44:47 +0500

    init

--- Basics/objects.js
+++ Basics/objects.js
@@ -1,12 +1,23 @@
 // ************************** Objects ********************
 
+let mySym = Symbol('mySym')
+
 let any = {
 
     name : "Atif" ,
-    age : 21
+    age : 21 ,
+    'Email' : "atifjs@gmail.com" ,
+    [mySym] : 'My Symbol',
 }
 // console.log(any);
 // console.log(any['name']);
 // console.log(any.age);
 // console.log(any.name);
 // console.log(any.any); // undefined
+// console.log(any.Email);
+// console.log( any[mySym]);
+// any.name = 'Afrid',
+// console.log(any);
+// Object.freeze(any)
+// any.age=19 
+// console.log(any);
