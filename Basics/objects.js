714b3fa9d3c537283f786d9b462a7b21d83113d6
Atif Afridi <atifafridi378@gmail.com>
2024-10-19 23:50:06 +0500

    init

--- Basics/objects.js
+++ Basics/objects.js
@@ -58,8 +58,8 @@
 
 const obj1 = {1 : 'a' , 2 : 'b' } ;
 const obj2 = {3 : 'c' , 4: 'd'}
-const obj3 = {...obj1,...obj2};
+const obj3 = {...obj1,...obj2}; // concatenating two objects
 console.log(obj3);
 
-obj4 = Object.assign( {},obj1 , obj2) ;
+obj4 = Object.assign( {},obj1 , obj2) ; // another way of concatenating
 console.log(obj4);
