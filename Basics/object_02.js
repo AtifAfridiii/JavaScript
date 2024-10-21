683e48430f14e7b7caec98199137066763470395
Atif Afridi <atifafridi378@gmail.com>
2024-10-21 23:33:58 +0500

    init

--- Basics/object_02.js
+++ Basics/object_02.js
@@ -1,7 +1,12 @@
 // ****************************** Objects de structure **************************
 const obj={
     "name": "John",
-    age : 19
+    age : 19,
+    arr : [
+    1,2,3,4,5
+    ]
 }
 console.log(obj);
 console.log(obj.age);
+console.log(obj.arr[3]);
+
