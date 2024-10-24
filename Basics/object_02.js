461c7bd91dd777a7d09eabfb7f0c0b1db3583baf
Atif Afridi <atifafridi378@gmail.com>
2024-10-24 23:53:26 +0500

    init

--- Basics/object_02.js
+++ Basics/object_02.js
@@ -6,6 +6,17 @@ const obj={
     1,2,3,4,5
     ]
 }
+
+const obj2 = {
+    name1 :  'Obj 2' ,
+    lan : 'js',
+    subject : {
+        name2 : 'Math',
+       teacher : 'xyz',
+       chapter : '12345',
+    }
+}
+
 // console.log(obj);
 // console.log(obj.age);
 // console.log(obj.arr[3]);
@@ -21,6 +32,12 @@ const obj={
 const { name, age } = obj;
 console.log(name); 
 console.log(age);
+
+const { name1, subject: { name2 , teacher ,chapter} } = obj2;
+console.log(name1);
+console.log(name2);
+console.log(chapter);
+
 
 
 
