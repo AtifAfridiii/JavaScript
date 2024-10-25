1910d816e56d16cda6b26ff926868c7855b8c722
Atif Afridi <atifafridi378@gmail.com>
2024-10-25 23:59:01 +0500

    init

--- Basics/functions/funcations_01.js
+++ Basics/functions/funcations_01.js
@@ -13,3 +13,14 @@ function add(num1 , num2){
     return num1 + num2
 }
 console.log(add(3,9));
+
+function login (name){
+    if(name === undefined){
+        console.log('please enter your name');
+        return 
+    }
+    
+    return `${name} jusss log in`
+}
+
+console.log(login('atif'));
