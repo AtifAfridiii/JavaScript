572c8291dd8efa757ce91fb77de145fc37b5fe85
Atif Afridi <atifafridi378@gmail.com>
2024-10-26 23:11:49 +0500

    init

--- Basics/functions/funcations_01.js
+++ Basics/functions/funcations_01.js
@@ -9,18 +9,55 @@
 // myname()
 
 
-function add(num1 , num2){
-    return num1 + num2
-}
-console.log(add(3,9));
+// function add(num1 , num2){
+//     return num1 + num2
+// }
+// console.log(add(3,9));
 
-function login (name){
-    if(name === undefined){
-        console.log('please enter your name');
-        return 
-    }
+// function login (name){
+//     if(name === undefined){
+//         console.log('please enter your name');
+//         return 
+//     }
     
-    return `${name} jusss log in`
+//     return `${name} jusss log in`
+// }
+
+// console.log(login('atif'));
+
+
+function RestOp(...num1){
+
+    return num1 
+
 }
 
-console.log(login('atif'));
+// console.log(RestOp(5,10,15,20,25));
+
+const cart = {
+    username : 'atif',
+    price : 12345
+
+
+}
+
+function handleobject(anything){
+console.log(`User name is ${anything.username} and price is ${anything.price}`);
+
+}
+
+// handleobject(cart)
+
+// handleobject({
+//     username : 'afridi',
+//     price : 111
+// })
+
+
+const arr = [1000,2000,300,4000,5000]
+function handlearray(anything){
+
+    return anything[0]
+
+}
+// console.log(handlearray(arr));
