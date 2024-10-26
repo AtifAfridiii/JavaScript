50fa60510b7182fd1274794e8ea16b5b16939920
Atif Afridi <atifafridi378@gmail.com>
2024-10-26 23:50:25 +0500

    init

---
+++ Basics/functions/scops.js
@@ -0,0 +1,17 @@
+// ********************************* scopes ************************************
+
+function one (){
+
+    const username = 'atif'
+    
+    function two(){
+        const username2 = 'afridi'
+        console.log(username);
+        
+    }
+   // console.log(username2);
+    
+two()
+
+}
+one()
