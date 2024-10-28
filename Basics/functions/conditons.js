cfac784c7ef10278046f45e4e32b1a2c2eaefbfe
Atif Afridi <atifafridi378@gmail.com>
2024-10-28 22:08:08 +0500

    init

---
+++ Basics/functions/conditons.js
@@ -0,0 +1,60 @@
+// ****************************************** Conditional Statements **********************************
+
+// let username = 'afridi'
+// let age = 25
+
+// if(username==='atif'){
+//     console.log(`${username} name matched`);
+// }else if(age===25){
+//     console.log('age is matched');
+    
+// }
+
+
+let month = 'nov'
+
+switch(month){
+    case 'jan' :
+        console.log('January');
+        break ;
+        
+    case 'feb' :
+        console.log('febuary');
+        break ;
+    case 'mar' :
+        console.log('march');
+        break ;
+    case 'apr' :
+        console.log('april');
+        break ;
+    case 'may' :
+        console.log('may');
+        break ;
+    case 'june' :
+        console.log('june');
+        break ;
+    case 'july' :
+        console.log('july');
+        break ;
+    case 'aug' :
+        console.log('august');
+        break ;
+    case 'sep' :
+        console.log('september');
+        break ;
+    case 'oct' :
+        console.log('october');
+        break ;
+    case 'nov' :
+        console.log('novemeber');
+        break ;
+
+        case 'dec' :
+            console.log('decemeber');
+            break ;
+        default :
+        console.log('No month');
+        break ;
+
+        
+}
