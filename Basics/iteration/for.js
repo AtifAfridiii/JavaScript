694dfd55506de96990b17a32900b0caf39c1262c
Atif Afridi <atifafridi378@gmail.com>
2024-10-28 23:47:07 +0500

    init

---
+++ Basics/iteration/for.js
@@ -0,0 +1,57 @@
+// ********************************* For of ************************
+
+let array = ['venom' , 'super man' , 'bat man']
+
+for (const value of array){
+ // console.log(value);
+
+}
+
+
+let program  = 'hello world'
+
+for(const num of program){
+   // console.log(num);
+    
+}
+
+
+let map = new Map()
+map.set('name' , 'john') 
+map.set('age', 21)
+
+for(const [key, val] of map){
+   // console.log(key,val);
+    
+}
+
+
+// ******************************** For in *********************************************
+
+
+let array1 = ['venom' , 'super man' , 'bat man']
+
+for(const key in array1){
+    // console.log(key,  array1[key]);
+    
+}
+
+let obj = {
+    name : 'john',
+    age : 21,
+    country : 'USA'
+}
+
+for(const key in obj){
+  // console.log(key,obj[key]);
+    
+}
+
+// ******************************* for each ************************************
+
+let arr = ['js','cpp','java','dart','ruby']
+arr.forEach((item,index,array)=>{
+    console.log(item,index,array);
+    
+})
+
---
+++ Basics/iteration/while.js
@@ -0,0 +1,25 @@
+// ****************************************** while looop *************************************
+
+let i = 1 ;
+while (i<=10) {
+    console.log(i);
+    i++
+}
+
+let array = ['venom' , 'super man' , 'bat man']
+
+let j = 0
+while (j <array.length) {
+    let ele = array[j]
+    console.log(ele);
+    j++
+}
+
+// *************************************** Do while ************************************
+
+
+let s  = 11 ;
+do {
+    console.log("value of "+s);
+    s++ ;
+}while(s<=10);
