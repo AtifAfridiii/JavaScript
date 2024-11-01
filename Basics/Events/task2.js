6d6d6667e9f0270b0028d3d034ef548267e5a083
Atif Afridi <atifafridi378@gmail.com>
2024-11-01 21:43:24 +0500

    task2

--- Basics/Events/task2.js
+++ Basics/Events/task2.js
@@ -1,15 +1,25 @@
+const randomColor = function() {
+    let color = '0123456789ABCDEF';
+    let hex = '#';
+    for (let i = 0; i < 6; i++) {
+        hex += color[Math.floor(Math.random() * 16)];
+    }
+    return hex;
+};
 
+let intervalID;
 
-const randomColor = function(){
-let color = '0123456789ABCDEF'
+const startChange = function() {
+    intervalID = setInterval(changebgColor, 1000);
 
-let hex = '#'
+    function changebgColor() {
+        document.body.style.backgroundColor = randomColor();
+    }
+};
 
-for (let i = 0; i < 6; i++) {
-   color+=hex[Math.floor(Math.random()*16)];
-    
-}
-return color ;
+const stopChange = function() {
+    clearInterval(intervalID); 
+};
 
-}
-
+document.querySelector('#start').addEventListener('click', startChange);
+document.querySelector('#stop').addEventListener('click', stopChange);
