77ad2a1de0abbd94679f76697fdb4643b65c7838
Atif Afridi <atifafridi378@gmail.com>
2024-11-02 22:20:56 +0500

    promises

--- Basics/promises/promise01.js
+++ Basics/promises/promise01.js
@@ -3,7 +3,7 @@
 const PromiseOne = new Promise(function(resolve,reject){
     setTimeout(function(){
      resolve();
-    console.log('promise 1');
+    // console.log('promise 1');
   
   
     },3000)
@@ -11,5 +11,41 @@ const PromiseOne = new Promise(function(resolve,reject){
 })
 
 PromiseOne.then(function(){
-    console.log('promise 1 resolved')
+    // console.log('promise 1 resolved')
+})
+
+// ******************************************************************************
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:'Atif',email:'atif!2334'});
+    },1000)
+}).then(function(user){
+   //  console.log(user);
+    
+})
+
+// *******************************************************************
+
+const promiseFour = new Promise(function(resolve , reject){
+    let error = false ;
+    if(!error){
+        resolve({name:'Atif Afridi' , age : 19 });  
+      }else{
+        reject('error');
+      }
+})
+
+promiseFour.then((user)=>{
+  console.log(user);
+  return user.name
+}).then((name)=>{
+    console.log(`Name is `+name);
+    return name.age
+}).catch((error)=>{
+    console.log(error);
+    
+}).finally(()=>{
+    console.log('something is happening');
+    
 })
