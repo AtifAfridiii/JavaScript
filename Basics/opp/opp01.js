912e1d11439151f3f8f6cb8a8fd3b1e5a479fad5
Atif Afridi <atifafridi378@gmail.com>
2024-11-03 23:46:23 +0500

    init

---
+++ Basics/opp/opp01.js
@@ -0,0 +1,12 @@
+
+
+// ************************************************* oop ************************************
+
+let name = 'Atif     ' ;
+
+String.prototype.Truelength = function(){
+
+console.log(`True lenght is ${this.trim().length}`)
+
+}
+name.Truelength()
