76e9277d1e45ffd66e138f4ecb53361630851660
Atif Afridi <atifafridi378@gmail.com>
2024-11-08 00:07:20 +0500

    init

--- Basics/objectProp.js
+++ Basics/objectProp.js
@@ -5,5 +5,16 @@ const val = Object.getOwnPropertyDescriptor(Math,'PI')
 console.log(val);
 
 const obj ={
-    
+  name : 'js',
+  available : true ,
+  PI : 3.14
 }
+
+console.log(Object.getOwnPropertyDescriptor(obj,'name'));
+
+Object.defineProperty(obj,'name',{
+    writable: false,
+     enumerable: false,
+      configurable: false
+})
+console.log(Object.getOwnPropertyDescriptor(obj,'name'));
